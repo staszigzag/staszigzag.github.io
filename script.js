b9f3372d940e8328d8919b4db700581fce85c315
staszigzag <stas.zigzag@gmail.com>
2019-03-10 13:21:01 +0300

    3

--- script.js
+++ script.js
@@ -6,21 +6,37 @@ var listItem = document.getElementsByClassName("list__wrapper")[0];
 
 
 function changeItem(){
+    var inputItem = this.parentElement.previousElementSibling.firstElementChild;
+    var labelItem = inputItem.nextElementSibling;
+    var textItem = labelItem.lastElementChild;
 
+    if(this.textContent == "Изменить"){
+    inputItem.innerText = textItem.innerText;
+    labelItem.style.display = "none";
+    inputItem.style.display = "";
+    inputItem.focus();
+    this.innerText = "Сохранить";
+    }else{
+        textItem.innerText = inputItem.innerText;
+        labelItem.style.display = "";
+        inputItem.style.display = "none";
+        this.innerText = "Изменить"
+    }
 }
 function deleteItem(evt){
-    console.dir(evt);
-    console.dir(this);
-    var itemList = this.parentElement.parentElement; //получить задачу из списка
-    itemList.classList.add("opacity");
-    setTimeout( function(){itemList.parentElement.removeChild(itemList)},500);
+    // console.dir(evt);
+    // console.dir(this);
+    var itemIslist = this.parentElement.parentElement; //получить задачу из списка
+    itemIslist.classList.add("opacity");
+    setTimeout( function(){itemIslist.parentElement.removeChild(itemIslist)},500);
 }
 function saveItem(){
-       
+   
 }
 function putListener(element){
-    var x = element.querySelector(".item__btn-delete").addEventListener("click", deleteItem);
-    
+    element.querySelector(".item__btn-delete").addEventListener("click", deleteItem);
+    element.querySelector(".item__btn-change").addEventListener("click", changeItem);
+    // element.querySelector(".item__btn-delete").addEventListener("click", deleteItem);
 }
 function creatItem(){ 
     var cloneTemplate = templateItem.cloneNode(true);
