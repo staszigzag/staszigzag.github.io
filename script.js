fcae5e99f1d17463fd1d41e954df4bf94df07eef
staszigzag <stas.zigzag@gmail.com>
2019-03-11 18:37:40 +0300

    4

--- script.js
+++ script.js
@@ -3,117 +3,87 @@ var btnAddItem = document.getElementsByClassName("creator-item__btn-add")[0];
 var inputAddItem = document.getElementsByClassName("creator-item__input")[0];
 var listItem = document.getElementsByClassName("list__wrapper")[0];
 
-
-
+// изменить или сохранить элемент списка-------------------------------------------------------------------------------
 function changeItem(){
     var inputItem = this.parentElement.previousElementSibling.firstElementChild;
     var labelItem = inputItem.nextElementSibling;
     var textItem = labelItem.lastElementChild;
 
-    if(this.textContent == "Изменить"){
+    if (this.textContent == "Изменить"){  //проверка на состояние кнопки
     inputItem.innerText = textItem.innerText;
     labelItem.style.display = "none";
     inputItem.style.display = "";
-    inputItem.focus();
+    inputItem.focus(); // добавить фокус на поле ввода
     this.innerText = "Сохранить";
     }else{
-        if((textItem.innerText != inputItem.innerText)&& (inputItem.innerText.trim() != "") ){
-            //проверка на изменения и пустую строку
+        if ((textItem.innerText != inputItem.innerText) && (inputItem.innerText.trim() != "")){ 
+            //проверка на изменения текста и пустую строку
             textItem.innerText = inputItem.innerText;
-            parseListItemAndWriteLocacStorage ();
+            parseListItemAndWriteLocacStorage();
         }
         labelItem.style.display = "";
         inputItem.style.display = "none";
         this.innerText = "Изменить";
-        
     }
 }
-function deleteItem(evt){
-    // console.dir(evt);
-    // console.dir(this);
-    var itemIslist = this.parentElement.parentElement; //получить задачу из списка
-    itemIslist.classList.add("opacity");
-    setTimeout( function(){itemIslist.parentElement.removeChild(itemIslist);
-                            parseListItemAndWriteLocacStorage ();},400);
+// удалить элемент из списка задач-------------------------------------------------------------------------------------
+function deleteItem(){
+    var itemIslist = this.parentElement.parentElement; //получить элемент из списка
+
+    itemIslist.classList.add("opacity");   //добавить класс с анимацией прозрачности
+    setTimeout(function()  { itemIslist.parentElement.removeChild(itemIslist);
+                            parseListItemAndWriteLocacStorage();
+                            },400); // анимация прозрачности длиться 300мс, а элемент удалим через 400мс
 }
-function putListener(element){
-    element.querySelector(".item__btn-delete").addEventListener("click", deleteItem);
-    element.querySelector(".item__btn-change").addEventListener("click", changeItem);
-}
+// создать новый элемент для списка -------------------------------------------------------------------------------------
 function creatItem(){ 
-    var cloneTemplate = templateItem.cloneNode(true);
+    var cloneTemplate = templateItem.cloneNode(true); // глубокое копирование
     var text = inputAddItem.innerText;
-    
-    if(text.trim() == ""){                     //проверка на пустую строку
+
+    if (text.trim() == ""){                     //проверка на пустую строку
         inputAddItem.innerText = "";
         inputAddItem.focus();
         }else{ 
         cloneTemplate.querySelector(".item__text p").innerText = text;
         putListener(cloneTemplate);
-        
         listItem.appendChild(cloneTemplate);
         inputAddItem.innerText = "";
         inputAddItem.focus();
         parseListItemAndWriteLocacStorage ();
-        
-    }
+        }
 }
-
-
-// creatItem
-// console.dir(cloneTemplate);
-btnAddItem.addEventListener("click", creatItem);
-document.addEventListener("change", parseListItemAndWriteLocacStorage );
-
-var arrItem
-var localListItem 
-
+// повесить обработчики на кнопки нового элемента ------------------------------------------------------------------------
+function putListener(element){
+    element.querySelector(".item__btn-delete").addEventListener("click", deleteItem);
+    element.querySelector(".item__btn-change").addEventListener("click", changeItem);
+}
+// запись в LocacStorage---------------------------------------------------------------------------------------------------
 function parseListItemAndWriteLocacStorage (){
     var arrLi = document.querySelectorAll(".item");
-    var arrInfo = {} ; //??????????????????????????????????????????
-    for (var i = 0; i < arrLi.length; i++) {
+    var arrObjectInfo = {}; 
+    for (var i = 0; i < arrLi.length; i++){
         var obj = {};
         obj.status = arrLi[i].querySelector(".item__chek").checked;
         obj.text = arrLi[i].querySelector(".item__text p").innerText;
-        arrInfo[i] = obj;
+        arrObjectInfo[i] = obj;
     }
-    localStorage.setItem("ToDoList", JSON.stringify(arrInfo));
-// console.dir(JSON.parse(localStorage.getItem("ToDoList")));
-
+    localStorage.setItem("ToDoList", JSON.stringify(arrObjectInfo));
 }
-function writeInHTML(){
-    var arrInfo = JSON.parse(localStorage.getItem("ToDoList"));
-    // if(arrInfo.ToDoList == "no list item") return;
-    for( var key in arrInfo){
+// проверка при старте на наличие записей, создание тестовой и их рендер---------------------------------------------------
+function parseStartLocalStorage(){
+    var arrObjectInfoOutLocal = JSON.parse(localStorage.getItem("ToDoList"));
+
+    if (!arrObjectInfoOutLocal){   //проверка на наличие записей, и присваение lorem 
+        arrObjectInfoOutLocal = {"0":{"status":false,"text":"беспробудную тупость, всегда легко спутать с гениальностью.... 😈 😈 😈"}};
+    }
+    for (var key in arrObjectInfoOutLocal){  //рендер всех сохраненных записей
         var cloneTemplate = templateItem.cloneNode(true);
-        cloneTemplate.querySelector(".item__text p").innerText = arrInfo[key].text;
-        cloneTemplate.querySelector(".item__chek").checked = arrInfo[key].status;
+        cloneTemplate.querySelector(".item__text p").innerText = arrObjectInfoOutLocal[key].text;
+        cloneTemplate.querySelector(".item__chek").checked = arrObjectInfoOutLocal[key].status;
         putListener(cloneTemplate);
         listItem.appendChild(cloneTemplate);
-        // inputAddItem.innerText = "";
-        // inputAddItem.focus();
-    }
-
-}
-
-
-function parseStartLocalStorage(){
-    if(localStorage.getItem("ToDoList")){
-    localListItem = JSON.parse(localStorage.getItem("ToDoList"));
-    console.dir(localListItem);
-    }else{
-    // localStorage.setItem("ToDoList", '{"ToDoList":"no list item"}')
-    localStorage.setItem("ToDoList", '{"0":{"status":false,"text":"беспробудную тупость, всегда легко спутать с гениальностью....   😈 😈 😈 "}}')
-    console.dir("no");
     }
 }
-
-
-parseStartLocalStorage();
-// parseListItemAndWriteLocacStorage ();
-writeInHTML();
-
-
-// console.dir({localStorage:"new", fgfg:"ff"} );
-// console.info({localStorage:"new", fgfg:"ff"} );
-// localStorage.new.z =2452254;
+btnAddItem.addEventListener("click", creatItem); // обработка нажатия на добавить
+document.addEventListener("change", parseListItemAndWriteLocacStorage); // запись изменений при смене чекбоксов
+parseStartLocalStorage(); //точка входа 
